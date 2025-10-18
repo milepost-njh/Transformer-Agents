49f9700cc250c8e0758b34bec274633f3bc7bf7a
ys <zhangyuesheng@kushimai.com>
2025-10-18 15:50:13 +0800

    减少epoch

--- train_tmp.py
+++ train_tmp.py
@@ -2006,7 +2006,7 @@ if __name__ == "__main__":
     # 模型训练超参数
     batch_size = 64  # 批处理数
     warmup_steps = 4000  # warmup steps数
-    epochs = 20  # 训练轮数
+    epochs = 15  # 训练轮数
     # learning_rate = 1.0           # 学习率
     # betas = (0.9, 0.98)           # Adam 的一阶矩（梯度均值）；二阶矩（梯度平方的均值）
     # eps = 1e-9                    # 防止除零错误的小常数
